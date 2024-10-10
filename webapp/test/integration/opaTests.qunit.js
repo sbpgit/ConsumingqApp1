57e002badf619a9c92a08d2b922c99dd15b8a5c7
Koti Tejaswi <tejaswikoti@sbpcorp.in>
2024-10-10 08:04:04 +0000

    consume

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["consume/consumingapp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"consume/consumingapp/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
